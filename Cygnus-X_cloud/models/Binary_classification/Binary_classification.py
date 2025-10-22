e5af2bc868a1f5aab60d98c3f2ccf5e2f7d85461
fujimoto <progaress21@gmail.com>
2025-10-22 15:15:03 +0900

    Add Binary_classification.py

---
+++ Cygnus-X_cloud/models/Binary_classification/Binary_classification.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+
+
+class Binary_classification(nn.Module):
+    def __init__(self, latent):
+        super(Conv3dAutoencoder, self).__init__()
+
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Conv3d(1, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm3d(32), nn.ReLU(True),
+            nn.Conv3d(32, 32, kernel_size=4, stride=2, padding=1), 
+            nn.BatchNorm3d(32), nn.ReLU(True),
+            nn.Conv3d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm3d(64), nn.ReLU(True),
+            nn.Conv3d(64, 64, kernel_size=3, stride=1, padding=1), 
+            nn.ReLU(True),
+            nn.Conv3d(64, 32, kernel_size=3, stride=1, padding=0), 
+            nn.ReLU(True),
+            nn.Flatten(),
+            nn.Linear(32 * 5 * 23 * 23, latent),  # Adjust the size based on the flattened output
+            nn.Linear(latent, 2)
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        return x
