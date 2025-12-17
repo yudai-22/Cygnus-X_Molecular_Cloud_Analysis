c8c9b84eeb4be02d1857ee74ac611948d95eb277
fujimoto <progaress21@gmail.com>
2025-12-17 15:02:16 +0900

    Add Dropout

--- Binary_classification/training/models/Binary_classification.py
+++ Binary_classification/training/models/Binary_classification.py
@@ -50,3 +50,56 @@ class Binary_classification(nn.Module):
         out = self.classifier[4](x)           # Sigmoid
         
         return out, latent_out
+
+
+
+class Binary_classification_Dropout(nn.Module):
+    def __init__(self, latent, 
+                 # input_depth, input_height, input_width
+                ):
+        super(Binary_classification, self).__init__()
+        
+        self.features = nn.Sequential(
+            nn.Conv3d(1, 16, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm3d(16),
+            nn.ReLU(True),
+            
+            nn.Conv3d(16, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm3d(32),
+            nn.ReLU(True),
+
+            nn.MaxPool3d(kernel_size=2, stride=2),
+            
+            nn.Conv3d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm3d(32),
+            nn.ReLU(True),
+            
+            nn.Conv3d(32, 16, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(True)
+        )
+
+        FINAL_FLATTEN_SIZE = 16 * 1 * 10 * 10 # 仮の値
+        
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(FINAL_FLATTEN_SIZE, latent),
+            nn.ReLU(True), 
+            nn.Linear(latent, 1),
+            nn.Sigmoid()
+        )
+
+    # def forward(self, x):
+    #     x = self.features(x)
+    #     x = self.classifier(x)
+    #     return x
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier[0](x) # Flatten
+        x = self.classifier[1](x) # Linear(..., latent) -> ここが欲しい値
+        
+        latent_out = self.classifier[2](x)    # ReLU
+        x = self.classifier[3](latent_out)    # Linear(latent, 1)
+        out = self.classifier[4](x)           # Sigmoid
+        
+        return out, latent_out
